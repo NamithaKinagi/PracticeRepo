a92f59fbf32d1bf10530ced8664cd7ec40a91637
Namitha Kinagi <nkinagi@Administrators-MacBook-Pro.local>
2024-05-28 23:43:08 +0530

    Exam score solution

---
+++ ExamScoreProblem/examScoreSolution.js
@@ -0,0 +1,14 @@
+function checkExam(array1, array2) {
+  let numberOfQs = array1.length;
+  let result = 0;
+  for (let i = 0; i < numberOfQs; i++) {
+    if (array2[i] === "") {
+    } else if (array1[i] === array2[i]) {
+      result += 4;
+    } else {
+      result -= 1;
+    }
+  }
+
+  return Math.max(result, 0);
+}
